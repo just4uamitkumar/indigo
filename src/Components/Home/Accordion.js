dc234198d299a465929016c9fa2aea1d14497e49
just4uamitkumar <just4uamitkuamr@gmail.com>
2020-12-05 13:41:44 +0530

    first commit

---
+++ src/Components/Home/Accordion.js
@@ -0,0 +1,33 @@
+import React, {useState, useRef } from "react";
+import { FaPlus, FaMinus } from 'react-icons/fa';
+
+
+function Accordion(props) {
+    const [setActive, setActiveState] = useState("");
+    const [setHeight, setHeightState] = useState("0px");
+
+    const content = useRef(null);
+
+    function toggleAccordion() {
+        setActiveState(setActive === "" ? "active" : "");
+        setHeightState(
+          setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+        );       
+    }
+    
+    
+ return (
+   <div className="accordionSection">    
+       <div className={`accordionTitle ${setActive}`} onClick={toggleAccordion}>
+           {props.title}
+           { setActive === "active" ? <FaMinus/> :  <FaPlus/>}
+        </div>     
+     
+     <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordItem">
+       <div className="accordCont">{props.content}</div>
+     </div>
+   </div>
+ );
+}
+
+export default Accordion;
---
+++ src/Components/Shared/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link, withRouter} from 'react-router-dom'
+
+const Header = () => {
+    const logo = 'LOGO'
+    return (
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">{logo}</Link>
+                </div>
+            </div>
+        </header>        
+    );
+}
+
+
+export default withRouter(Header);
